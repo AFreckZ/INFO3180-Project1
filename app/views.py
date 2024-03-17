6215e71072203f9d8caa219fb9fc74eb3f071066
AFreckZ <andrew.freckleton21@gmail.com>
2024-03-17 16:27:08 -0500

    Update views

--- app/views.py
+++ app/views.py
@@ -4,10 +4,14 @@ Jinja2 Documentation:    https://jinja.palletsprojects.com/
 Werkzeug Documentation:  https://werkzeug.palletsprojects.com/
 This file contains the routes for your application.
 """
-
-from app import app
-from flask import render_template, request, redirect, url_for
-
+import os
+from app import app, db
+from flask import render_template, request, redirect, url_for, flash
+from .forms import PropertyForm
+from .models import Property
+from . import db
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
 
 ###
 # Routing for your application.
@@ -22,7 +26,7 @@ def home():
 @app.route('/about/')
 def about():
     """Render the website's about page."""
-    return render_template('about.html', name="Mary Jane")
+    return render_template('about.html', name="Andrew Freckleton")
 
 
 ###
@@ -61,3 +65,45 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+
+
+# Define the routes and their corresponding view functions
+@app.route('/properties/create', methods=['GET', 'POST'])
+def form():
+    form = PropertyForm()
+    if request.method == 'POST':
+        Title = request.form['Title']
+        Description = request.form['Description']
+        No_Room = request.form['No_Room']
+        No_Bathrooms = request.form['No_Bathrooms']
+        Location = request.form['Location']
+        Price = request.form['Price']
+        Property_Type = request.form['Property_Type']
+
+        photo_file = request.files['photo']
+        filename = secure_filename(photo_file.filename)
+        photo_file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+        # Create new property object and add to database
+        new_prop = Property(Title=Title, Description=Description, No_Room=No_Room, No_Bathrooms=No_Bathrooms, Location=Location, Price=Price, Property_Type=Property_Type, Photo=filename)
+        db.session.add(new_prop)
+        db.session.commit()
+
+        flash('Property added successfully', 'success')
+        return redirect(url_for('properties'))
+
+    return render_template('form.html',form=form)
+
+@app.route('/properties')
+def properties():
+    properties = Property.query.all()
+    return render_template('properties.html', properties=properties)
+
+@app.route('/properties/<propertyid>')
+def property(propertyid):
+    property = Property.query.get(propertyid)
+    return render_template('property.html', property=property)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(os.path.join(os.getcwd(), app.config['UPLOAD_FOLDER']), filename)
